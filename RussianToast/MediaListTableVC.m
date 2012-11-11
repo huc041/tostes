40c4dff264bdd3a810460fb85089467db10fc16c
john <jovanny041@gmail.com>
2012-11-11 21:30:51 +0400

    экраны подгрупп и медиалиста

--- RussianToast/MediaListTableVC.m
+++ RussianToast/MediaListTableVC.m
@@ -39,9 +39,10 @@
     [super viewDidLoad];
         
     table = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height - 49.0f - 46.0f)];
-    table.backgroundColor = [UIColor darkGrayColor];
+    table.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"backgrnd.png"]];
     table.delegate = self;
     table.dataSource = self;
+    [table setSeparatorStyle:UITableViewCellSeparatorStyleNone];
     [self.view addSubview:table];
     [table release];
 }
@@ -72,6 +73,10 @@
 	{
 		cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:CellIdentifier] autorelease];
 		[cell setSelectionStyle:UITableViewCellSelectionStyleNone];
+        
+        cell.textLabel.backgroundColor = [UIColor clearColor];
+        cell.textLabel.textColor = RGB_Color(66.0f, 42.0f, 2.0f, 2.0f);
+        cell.textLabel.font = [UIFont fontWithName:@"MyriadPro-Bold" size:16];
 	}
     
     MediaDB *mediaDB = [mediaArray objectAtIndex:indexPath.row];
--- RussianToast/SubGroupsVC.m
+++ RussianToast/SubGroupsVC.m
@@ -43,9 +43,10 @@
     self.navigationController.navigationBar.topItem.title = @"Главная";
     
     table = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height - 49.0f - 46.0f)];
-    table.backgroundColor = [UIColor darkGrayColor];
+    table.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"backgrnd.png"]];
     table.delegate = self;
     table.dataSource = self;
+    [table setSeparatorStyle:UITableViewCellSeparatorStyleNone];
     [self.view addSubview:table];
     [table release];
 }
@@ -76,6 +77,10 @@
 	{
 		cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:CellIdentifier] autorelease];
 		[cell setSelectionStyle:UITableViewCellSelectionStyleNone];
+        
+        cell.textLabel.backgroundColor = [UIColor clearColor];
+        cell.textLabel.textColor = RGB_Color(66.0f, 42.0f, 2.0f, 2.0f);
+        cell.textLabel.font = [UIFont fontWithName:@"MyriadPro-Bold" size:16];
 	}
     
     GroupDB *subGroup = [self.detailFetchResultController.fetchedObjects objectAtIndex:indexPath.row];
@@ -132,7 +137,7 @@
     
     NSString *predicate = [NSString stringWithFormat:@"idParent == %@",idParent];
     [request setSortDescriptors:[NSArray arrayWithObjects:
-                                     [[[NSSortDescriptor alloc] initWithKey:@"id" ascending:NO] autorelease],
+                                     [[[NSSortDescriptor alloc] initWithKey:@"name" ascending:YES] autorelease],
                                      nil]];
     [request setPredicate:[NSPredicate predicateWithFormat:predicate]];
 
